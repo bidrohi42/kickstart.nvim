5faf72b51155575de3f6ca4190224e0a19b5b392
Saib <suhaibhossain77@example.com>
2025-12-03 01:02:34 +0600

    Added a separate file for options.

---
+++ lua/config/options.lua
@@ -0,0 +1,77 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- [[ Setting options ]]
+-- See `:help vim.o`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
+
+-- Left column and similar settings
+vim.opt.number = true -- display line numbers
+vim.opt.relativenumber = true -- display relative line numbers
+vim.opt.numberwidth = 2 -- set width of line number column
+vim.opt.signcolumn = 'yes' -- always show sign column
+vim.opt.wrap = false -- display lines as single line
+vim.opt.scrolloff = 10 -- number of lines to keep above/below cursor
+vim.opt.sidescrolloff = 8 -- number of columns to keep to the left/right of cursor
+
+-- Tab spacing/behavior
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.shiftwidth = 4 -- number of spaces inserted for each indentation level
+vim.opt.tabstop = 4 -- number of spaces inserted for tab character
+vim.opt.softtabstop = 4 -- number of spaces inserted for <Tab> key
+vim.opt.smartindent = true -- enable smart indentation
+vim.opt.breakindent = true -- enable line breaking indentation
+
+-- General Behaviors
+vim.opt.backup = false -- disable backup file creation
+vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
+vim.opt.fileencoding = 'utf-8' -- set file encoding to UTF-8
+vim.opt.mouse = 'a' -- enable mouse support
+vim.opt.showmode = false -- hide mode display
+vim.opt.splitbelow = true -- force horizontal splits below current window
+vim.opt.splitright = true -- force vertical splits right of current window
+vim.opt.termguicolors = true -- enable term GUI colors
+vim.opt.timeoutlen = 1000 -- set timeout for mapped sequences
+vim.opt.undofile = true -- enable persistent undo
+vim.opt.updatetime = 100 -- set faster completion
+vim.opt.writebackup = false -- prevent editing of files being edited elsewhere
+vim.opt.cursorline = true -- highlight current line
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+
+-- Searching Behaviors
+vim.opt.hlsearch = true -- highlight all matches in search
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true -- ignore case in search
+vim.opt.smartcase = true -- match case if explicitly stated
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--
+--  Notice listchars is set using `vim.opt` instead of `vim.o`.
+--  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-options-guide`
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
