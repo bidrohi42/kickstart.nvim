10f4ca100d0698f7d677dacfbcc73f672e9adb33
Saib <suhaibhossain77@example.com>
2025-12-03 01:49:53 +0600

    Added ftplugin and after/ftplugin for lua

---
+++ ftplugin/lua.lua
@@ -0,0 +1,3 @@
+vim.bo.shiftwidth = 2  -- number of spaces inserted for each indentation level
+vim.bo.tabstop = 2     -- number of spaces inserted for tab character
+vim.bo.softtabstop = 2 -- number of spaces inserted for <Tab> key
